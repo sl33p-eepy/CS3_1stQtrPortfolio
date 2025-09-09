2f380c44dd76c2df5a35b08c1d7b3f9c7a7fcdb6
sl33py-eepy <b2029mcmcarpiso@pshs.edu.ph>
2025-09-09 14:31:16 +0800

    hw

---
+++ new.js
@@ -0,0 +1,25 @@
+/*for (i=1; i < 10; i++){
+    console.log(i)
+}
+for (i=1; i < 20; i++){
+    if (i % 2 == 0){
+        console.log(i)
+    }
+}
+
+grades= {99, 100, 97, 82, 96}
+grades.push("kangkong")
+grades.unshift()
+
+/*for (let i = 0; i < grades.length; i++) {
+    console.log(grades[i])
+}
+
+for (let grade of grades) {
+    console.log(grade)
+}
+grades.forEach(print_grade)
+function print_grade(grade){
+    console.log(grade)
+}*/
+
